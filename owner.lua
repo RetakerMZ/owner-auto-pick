381cba324779becb4737a8931c40f873057f62a8
RetakerMZ <reiki290@gmail.com>
2022-10-04 00:01:06 +0800

    Update owner.lua

--- owner.lua
+++ owner.lua
@@ -1,9 +1,9 @@
-owner = "Colroon"
+owner = {"Colroon"}
 found = false
 while not found do
     say("Waiting For Owner")
     for _, plr in pairs(getPlayers()) do
-        for _,v in pairs(items) do
+        for _,v in pairs(owner) do
             if plr.name:upper() == v:upper() then
                 found = true
                 say("`2Owner ditemukan,SC By `4YASUGAMI")
